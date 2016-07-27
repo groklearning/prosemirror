f3ea8c9ba4303bdb3b07e7e87a039e2d458efc44
Jim Mussared <jim.mussared@gmail.com>
2016-07-27 16:23:28 +1000

    Add a build to produce IE10-compatible prosemirror.min.js.

---
+++ release/release.js
@@ -0,0 +1,19 @@
+const edit = require("../src/edit")
+const model = require("../src/model")
+const schema = require("../src/schema-basic")
+const table = require("../src/schema-table")
+const setup = require("../src/example-setup")
+const menu = require("../src/menu")
+const markdown = require("../src/markdown")
+const inputrules = require("../src/inputrules")
+
+module.exports = {
+  edit: edit,
+  model: model,
+  schema: schema,
+  table: table,
+  setup: setup,
+  menu: menu,
+  markdown: markdown,
+  inputrules: inputrules
+};
